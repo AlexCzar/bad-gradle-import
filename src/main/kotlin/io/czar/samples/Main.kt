b05b24c3c89c4630d45eac5af84a0ebb96b023ff
AlexCzar <AlexCzar@users.noreply.github.com>
2017-11-10 16:30:39 +0400

    * polish, make error appear * add readme

--- src/main/kotlin/io/czar/samples/Main.kt
+++ src/main/kotlin/io/czar/samples/Main.kt
@@ -1,6 +1,13 @@
 package io.czar.samples
 
+import org.slf4j.LoggerFactory
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
 
 fun main(args: Array<String>) {
-	println("Hi! I'm here")
+	val app = SpringApplication.run(Application::class.java, *args)
+	LoggerFactory.getLogger("Application").info("meow")
 }
+
+@SpringBootApplication
+class Application
